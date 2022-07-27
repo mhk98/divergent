223c15330f670ece6fc259b79ff70ea8961f21bc
Md Mohsin Kabir <mohsinkabirseo@gmail.com>
2022-07-27 18:16:06 +0600

    navbar

--- src/Pages/Navbar.js
+++ src/Pages/Navbar.js
@@ -44,18 +44,24 @@ const Navbar = () => {
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
       <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Home</a></li>
-        <li><a>Exclusive</a></li>
+      <li>
+        <a>
+      <FontAwesomeIcon className='text-2xl' icon={faHouse}></FontAwesomeIcon>
+      <span className='block'>Home</span>
+      </a>
+</li>
       </ul>
     </div>
    
   </div>
   <div class="navbar-center hidden lg:flex">
     <ul class="menu menu-horizontal p-0">
-       <div> 
-       <FontAwesomeIcon className='text-2xl' icon={faHouse}></FontAwesomeIcon>
-        <li className='list-none'><a>Home</a></li>
-        </div>
+    <li>
+        <a>
+      <FontAwesomeIcon className='text-2xl' icon={faHouse}></FontAwesomeIcon>
+      <span className='block'>Home</span>
+      </a>
+</li>
         <li><a>Exclusive</a></li>
     </ul>
   </div>
